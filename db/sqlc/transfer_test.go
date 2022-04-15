8da66b2c00941c0690064d67421dadb3be57b6a0
郝云飞 <haoyf@botech.com.cn>
2022-04-15 20:59:50 +0800

    transfer 测试编写

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -1,9 +1,83 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"database/sql"
+	"testing"
 
-func TestCreateTransfer(t *testing.T) {
+	"github.com/amyunfei/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, transfer.FromAccountID, account1.ID)
+	require.Equal(t, transfer.ToAccountID, account2.ID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreateAt)
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t)
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer2)
+
+	require.Equal(t, transfer1.ID, transfer2.ID)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.Equal(t, transfer1.CreateAt, transfer2.CreateAt)
+}
+
+func TestListTransfer(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTransfer(t)
+	}
+
+	arg := ListTransferParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	transfers, err := testQueries.ListTransfer(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
+
+func TestDeleteTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+
+	err := testQueries.DeleteTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err)
+
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, transfer2)
 }
