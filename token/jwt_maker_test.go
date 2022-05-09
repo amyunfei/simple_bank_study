547992db6e1767c35efbdf6b586af2a497bd31c3
郝云飞 <haoyf@botech.com.cn>
2022-05-09 20:12:07 +0800

    添加 jwtmaker 测试

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -2,22 +2,36 @@ package token
 
 import (
 	"testing"
+	"time"
+
+	"github.com/amyunfei/simplebank/util"
+	"github.com/stretchr/testify/require"
 )
 
 func TestJWTMaker(t *testing.T) {
-	// maker, err := NewJWTMaker(util.RandomString(32))
-	// require.NoError(t, err)
+	maker, err := NewJWTMaker(util.RandomString(32))
+	require.NoError(t, err)
 
-	// username := util.RandomOwner()
-	// duration := time.Minute
+	username := util.RandomOwner()
+	duration := time.Minute
 
-	// 	issuedAt := time.Now()
-	// 	expiredAt := issuedAt.Add(duration)
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
 
-	// 	token, err := maker.CreateToken(username, duration)
-	// 	require.NoError(t, err)
-	// 	require.NotEmpty(t, token)
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
 
-	// 	payload, err := maker.VerifyToken(token)
-	// 	require.NoError(t, err)
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	//
 }
